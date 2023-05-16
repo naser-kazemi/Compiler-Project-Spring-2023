faa6ec38692efcfece2929b47ad2c560de17559c
naser-kazemi <naserkazemi2002@gmail.com>
2023-05-16 16:37:27 +0330

    update utils.py

--- utils.py
+++ utils.py
@@ -29,6 +29,54 @@ class TokenType(Enum):
         return self.value
 
 
+rule_dict = {
+    "Program": [["Declaration-list"]],
+    "Declaration-list": [["Declaration", "Declaration-list"], ['epsilon']],
+    "Declaration": [["Declaration-initial", "Declaration-prime"]],
+    "Declaration-initial": [["Type-specifier", "ID"]],
+    "Declaration-prime": [["Fun-declaration-prime"], ["Var-declaration-prime"]],
+    "Var-declaration-prime": [[";"], ["[", "NUM", "]", ";"]],
+    "Fun-declaration-prime": [["(", "Params", ")", "Compound-stmt"]],
+    "Type-specifier": [["int"], ["void"]],
+    "Params": [["int", "ID", "Param-prime", "Param-list"], ["void"]],
+    "Param-list": [[",", "Param", "Param-list"], ['epsilon']],
+    "Param": [["Declaration-initial", "Param-prime"]],
+    "Param-prime": [["[", "]"], ['epsilon']],
+    "Compound-stmt": [["{", "Declaration-list", "Statement-list", "}"]],
+    "Statement-list": [["Statement", "Statement-list"], ['epsilon']],
+    "Statement": [["Expression-stmt"], ["Compound-stmt"], ["Selection-stmt"], ["Iteration-stmt"], ["Return-stmt"]],
+    "Expression-stmt": [["Expression", ";"], ["break", ";"], [";"]],
+    "Selection-stmt": [["if", "(", "Expression", ")", "Statement", "else", "Statement"]],
+    "Iteration-stmt": [["repeat", "Statement", "until", "(", "Expression", ")"]],
+    "Return-stmt": [["return", "Return-stmt-prime"]],
+    "Return-stmt-prime": [[";"], ["Expression", ";"]],
+    "Expression": [["Simple-expression-zegond"], ["ID", "B"]],
+    "B": [["=", "Expression"], ["[", "Expression", "]", "H"], ["Simple-expression-prime"]],
+    "H": [["=", "Expression"], ["G", "D", "C"]],
+    "Simple-expression-zegond": [["Additive-expression-zegond", "C"]],
+    "Simple-expression-prime": [["Additive-expression-prime", "C"]],
+    "C": [["Relop", "Additive-expression"], ['epsilon']],
+    "Relop": [["<"], ["=="]],
+    "Additive-expression": [["Term", "D"]],
+    "Additive-expression-prime": [["Term-prime", "D"]],
+    "Additive-expression-zegond": [["Term-zegond", "D"]],
+    "D": [["Addop", "Term", "D"], ['epsilon']],
+    "Addop": [["+"], ["-"]],
+    "Term": [["Factor", "G"]],
+    "Term-prime": [["Factor-prime", "G"]],
+    "Term-zegond": [["Factor-zegond", "G"]],
+    "G": [["*", "Factor", "G"], ['epsilon']],
+    "Factor": [["(", "Expression", ")"], ["ID", "Var-call-prime"], ["NUM"]],
+    "Var-call-prime": [["(", "Args", ")"], ["Var-prime"]],
+    "Var-prime": [["[", "Expression", "]"], ['epsilon']],
+    "Factor-prime": [["(", "Args", ")"], ['epsilon']],
+    "Factor-zegond": [["(", "Expression", ")"], ["NUM"]],
+    "Args": [["Arg-list"], ['epsilon']],
+    "Arg-list": [["Expression", "Arg-list-prime"]],
+    "Arg-list-prime": [[",", "Expression", "Arg-list-prime"], ['epsilon']]
+}
+
+
 def get_token_type(char):
     if char.isdigit():
         return TokenType.NUM
@@ -55,3 +103,28 @@ def read_grammar_data():
         first = data["first"]
         follow = data["follow"]
         return terminals, non_terminals, first, follow
+
+
+class Node:
+
+    def __init__(self, name):
+        self.name = name
+        self.child = None
+
+    def add_child(self, child):
+        self.child = child
+
+
+class TransitionDiagram:
+
+    def __init__(self, rule):
+        self.rule = rule
+        self.nodes = []
+        self.start = None
+        self.accept = None
+        self.createTD()
+
+    def createTD(self):
+        self.start = self.rule[0]
+        rhs = self.rule[1]
+
